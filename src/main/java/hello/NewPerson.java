66fbf337238def99f99ff5dd4eaca7a9e36e621e
mpietrewicz <torsenawd99>
2019-07-22 14:08:29 +0200

    Zwracanie zwykłej klasy przez http (json)

---
+++ src/main/java/hello/NewPerson.java
@@ -0,0 +1,16 @@
+package hello;
+
+class NewPerson {
+    private String name = "NO NAME";
+
+    public NewPerson() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
